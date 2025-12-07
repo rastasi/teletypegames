f47e3e48e61d1df4939e88b64fffe0b598a14e4e
Zsolt Tasnadi <rastasi@gmail.com>
2025-12-07 12:06:23 +0100

    platfom json name fix

--- domain/model.software.go
+++ domain/model.software.go
@@ -11,6 +11,6 @@ type Software struct {
 	Desc     string    `gorm:"type:text" json:"desc"`
 	Site     string    `gorm:"size:255" json:"site"`
 	License  string    `gorm:"size:128" json:"license"`
-	Platform string    `gorm:"size:128" json:"license"`
+	Platform string    `gorm:"size:128" json:"platform"`
 	Releases []Release `json:"releases"`
 }
